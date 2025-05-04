c6c04ac657ef7c98de296432eb80678ffc6b8cb1
Doroshchuk-Levitsky Gleb Sergeevich <doroshchuk@fenix-it.ru>
2025-05-04 23:01:54 +0300

    Обновление документации, просчета времени выполнения, оптимизации компилятора

--- Varble/Varble.cpp
+++ Varble/Varble.cpp
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <codecvt>
 #include <fcntl.h>
+#include "windows.h"
+#include <chrono>
 #include "VarClass.h"
 #include "Machine.h"
 #include "Parser.h"
@@ -12,27 +14,38 @@ using namespace std;
 
 int main()
 {
+    //Поднятие приоритета процесса
+    SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_TIME_CRITICAL);
+
+    //Кодировка Unicode для читаемых, записывемых и отображаемых символов
     Unicode();
 
     try {
-        clock_t start = clock();
+
         map<wstring, Var> map;
         Machine mchn(map, false);
 
+        //Загружаем и парсим исходный код
+        auto begin = chrono::high_resolution_clock::now();
         Parser p(L"file.vrb");
         p.fileLoad();
         p.parse(mchn);
+        auto end = chrono::high_resolution_clock::now();
+        auto elapsed_ms = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+        std::wcout << L"Время обработки исходного кода: " << to_wstring((double)elapsed_ms.count() / 1000000.0) << L"sec\n";
 
+        //Выполняем код
+        begin = chrono::high_resolution_clock::now();
         mchn.prepare();
-        wcout << endl << mchn.go() << endl;
-
-        clock_t end = clock();
-        double seconds = (double)(end - start) / CLOCKS_PER_SEC;
-        wcout << seconds << L" sec." << endl;
+        mchn.go();
+        end = chrono::high_resolution_clock::now();
+        elapsed_ms = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+        std::wcout << L"Время выполнения: " << to_wstring((double)elapsed_ms.count() / 1000000.0) << L"sec\n";
     }
     catch (const std::wstring& error_message) {
         wcout << endl << error_message << endl;
     }
+
     system("pause");
     return 0;
 }
